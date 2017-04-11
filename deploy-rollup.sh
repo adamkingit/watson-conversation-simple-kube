d0f953b83ae737cfebc12f37a7435c7308814c4a
Adam King <rak@us.ibm.com>
2017-04-11 18:41:12 -0400

    fix script names
    Signed-off-by: Adam King <rak@us.ibm.com>

--- deploy-rollup.sh
+++ deploy-rollup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Install Bluemix CLI"
-. ./install_bx.sh
+. ./install-bx.sh
 if [ $? -ne 0 ]; then
   echo "Failed to install Bluemix Container Service CLI prerequisites"
   exit 1
 fi
 
 echo "Login to Bluemix"
-./bx_login.sh
+./bx-login.sh
 if [ $? -ne 0 ]; then
   echo "Failed to authenticate to Bluemix Container Service"
   exit 1
